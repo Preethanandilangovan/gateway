4b3bd9c67a795ac7c48589981827e35886671f06
Preethanandilangovan <preethanandilangovan@gmail.com>
2025-01-07 00:09:36 +0530

    updated 07-01-2025 00:09 by preethanand

--- src/main/java/com/payment/gateway/api/controller/EntryController.java
+++ src/main/java/com/payment/gateway/api/controller/EntryController.java
@@ -2,8 +2,8 @@ package com.payment.gateway.api.controller;
 
 
 import com.payment.gateway.api.Model.PaymentInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -12,29 +12,47 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 public class EntryController {
 
     @Autowired
     PaymentInfo info;
 
-
+    private static final Logger logger = LogManager.getLogger(EntryController.class);
 
     @CrossOrigin
     @GetMapping("/status")
     public ResponseEntity<PaymentInfo> getStatus() {
-        Logger logger = LoggerFactory.getLogger(EntryController.class);
+
         ResponseEntity<PaymentInfo> response ;
         try {
-            logger.debug("Entered");
+            logger.info("Entered");
             info.setData(Thread.activeCount());
             response =  new ResponseEntity<PaymentInfo>(info, HttpStatus.OK);
         } catch (Exception ex) {
             logger.error(ex.getLocalizedMessage());
             response =  new ResponseEntity<PaymentInfo>(info, HttpStatus.INTERNAL_SERVER_ERROR);
-        } finally {
-            logger = null;
+        } finally
+        {
+
         }
         return response;
+    }
+
+    @GetMapping("/uuid")
+    public ResponseEntity<String> getUUID()
+    {
+        try
+        {
+            logger.info("Entered");
+        }
+        finally
+        {
+
+        }
+
+        return new ResponseEntity<String>(UUID.randomUUID().toString(), HttpStatus.OK);
     }
 }
